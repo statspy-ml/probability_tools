fa76f9a73dd7dd374948becf0b934d173f229ac6
statspy-ml <rodrigo.pp.toledo@gmail.com>
2022-02-12 18:00:17 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statspy_probability',
+      version='0.1a',
+      description='Gaussian and Binomial distributions',
+      packages=['statspy_probability'],
+      author = 'Rodrigo Toledo',
+      author_email = 'rodrigo.pp.toledo@gmail.com',
+      zip_safe=False)
